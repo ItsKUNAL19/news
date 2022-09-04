70de01dd7ed1a5368a1162773f3cb9c3e3f33ebd
kunal kumar <kunalkumar@kunals-MacBook-Air.local>
2022-09-04 12:29:08 +0530

    init

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -15,10 +15,10 @@ export class Navbar extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-              </li>
-              <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+              </li> */}
+              {/* <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
               <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
               <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
               <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
@@ -27,9 +27,9 @@ export class Navbar extends Component {
               <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
               
               
-             
+              */}
             </ul>
-            
+             
           </div>
         </div>
       </nav>
